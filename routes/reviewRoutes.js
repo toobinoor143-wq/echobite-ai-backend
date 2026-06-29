12776f9718253e6acb057e481c35b0c0866be017
Mahsam Iftikhar <malik_mahsam@hotmail.com>
2026-06-29 23:06:32 +0500

    MAIN :: REVIEW MODEL FIX

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Review = require("../models/review");
+const Review = require("../models/Review");
 
 //POST /api/review/add
 
